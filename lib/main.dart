4912ac5ac250ec7f0ceeb954897f6aa03dc53870
muham <muhammadaaa727@gmail.com>
2023-01-26 01:08:01 +0300

    remove all todo classes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:temp/todos/todos_page.dart';
 
 void main() => runApp(const MyApp());
 
@@ -10,7 +9,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Amplified Todo',
-      home: TodosPage(),
+      home: Scaffold(),
     );
   }
 }
